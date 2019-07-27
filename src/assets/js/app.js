8af9120ed2bd94b6d0e22a34a0af648e202856b3
Sarah Russell <sarah@skrdesign.com>
2019-07-26 20:13:34 -0600

    Slide out
    Added left tags standings slide out to nav

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -22,9 +22,11 @@ $(window).on("scroll", function(e){
         if ($(window).scrollTop() >= stickyTop) {
             $(".sticky_nav").css({position: "fixed", top: "0px"});
             $(".home_body").css({padding: "5.2rem 0 2rem 0"});
+            $(".side_slide_out").css({position: "fixed"});
         } else {
             $(".sticky_nav").css({position: "relative", top: "0px"});
             $(".home_body").css({padding: "0 0 2rem 0"});
+            $(".side_slide_out").css({position: "relative"});
         }
 });
 
@@ -33,4 +35,9 @@ $(".dropbtn").on("click", function() {
   $(".main_nav__mobile_menu_arrows").toggleClass("show-inline hidden");
   $(".main_nav__mobile_menu_close").toggleClass("show-inline hidden");
   $(".dropbtn").toggleClass("yellow");
+});
+
+$(".side_slide_out__title").on("click", function() {
+  $(".side_slide_out").toggleClass("open closed");
+  $(".side_slide_out__title").toggleClass("black");
 });
